35b9d8b4179e45e664be91d66a656fd38a0a14d4
bgangioni <bgangioni@yahoo.com>
2018-08-13 02:35:22 -0300

    initial draft main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]){
+    // Variables
+    // Status struct, file, memory?
+
+    // Read file input (file, memory)
+
+    // Process instructions (status, memory)
+
+    return 0;
+}
